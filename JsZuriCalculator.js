c6415e6c47c1fc892f559597571820ce0acbe6d2
damselsimi <damselsimi@gmail.com>
2022-06-25 07:55:14 +0100

    first commit

---
+++ JsZuriCalculator.js
@@ -0,0 +1,11 @@
+
+    let x = 5
+    let y = 2
+    let z = x+y;
+    let a = x-y;
+    let b = x*y;
+    let c = x/y;
+    console.log("the sum of given numbers is: " + z);
+    console.log("the difference of given numbers is: " + a);
+    console.log("the product of given numbers is: " + b);
+    console.log("the quotient of given numbers is: " + c)
